65dcaf96d8b2cb8681c0f096f222006492fb9bff
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-02-21 16:43:51 +0100

    fac - simile

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 const km = prompt('Inserisci i kilometri da percorrere:');
+const nomePass = prompt('Inserisci il Nome del passeggero:');
 const eta = prompt("inserisci l'età del passeggero:");
 const costoKm = 0.21;
 let costoBiglietto = km * costoKm;
-let sconto 
+let sconto
+let bigliettoScontato
+const outputNome = document.getElementById ('nome-pass');
+const outputKm = document.getElementById ('km');
+const outputSconto = document.getElementById ('sconto');
+const outputEuro = document.getElementById ('prezzo');
+
+outputNome.innerHTML = nomePass;
+outputKm.innerHTML = km;
 
 if (eta < 18) {
     // costoBiglietto = (costoBiglietto / 100) * 80;
     sconto =  ((costoBiglietto * 20) / 100);
-    costoBiglietto = costoBiglietto - sconto;    
-} else if (eta > 65) {
+    bigliettoScontato = costoBiglietto - sconto;   
+
+    var Biglietto2Decimali = Math.round((bigliettoScontato + Number.EPSILON) * 100) / 100;
+    var sconto2Decimali = Math.round((sconto + Number.EPSILON) * 100) / 100;
+    
+    outputSconto.innerHTML = `-20% (Minorenni) = ${sconto2Decimali} €`;
+    outputEuro.innerHTML = `${Biglietto2Decimali} €`;
+
+} else if (eta >= 65) {
     // costoBiglietto = (costoBiglietto / 100) * 60;
     sconto =  ((costoBiglietto * 40) / 100);
-    costoBiglietto = costoBiglietto - sconto;    
-} 
+    bigliettoScontato = costoBiglietto - sconto;   
 
-var Biglietto2Decimali = Math.round((costoBiglietto + Number.EPSILON) * 100) / 100;
-const outputEuro = document.getElementById ('costo-finale');
-outputEuro.innerHTML = 'Il prezzo totale del viaggio è di ' + Biglietto2Decimali + ' €';
+    var Biglietto2Decimali = Math.round((bigliettoScontato + Number.EPSILON) * 100) / 100;
+    var sconto2Decimali = Math.round((sconto + Number.EPSILON) * 100) / 100;
+    
+    outputSconto.innerHTML = `-40% (Over 65) = ${sconto2Decimali} €`;
+    outputEuro.innerHTML = `${Biglietto2Decimali} €`;
+
+} else {
+    var Biglietto2Decimali = Math.round((costoBiglietto + Number.EPSILON) * 100) / 100;
+    outputSconto.innerHTML = `-`;
+    outputEuro.innerHTML = `${Biglietto2Decimali} €`;
+}
+
